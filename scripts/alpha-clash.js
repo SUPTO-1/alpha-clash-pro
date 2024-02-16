4bb8d483aef2522048b2dbfd46bc66c8cfc28472
SUPTO-1 <jahidulislamsupta9@gmail.com>
2024-02-17 01:17:39 +0600

    complete ggame

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,3 +1,40 @@
+function handleKeyboardEvent(event)
+{
+    const playerPress = event.key;
+    const currentAlphabet = document.getElementById('current-screen').innerText;
+    const expectedPlayerPress = currentAlphabet.toLowerCase();
+    console.log(expectedPlayerPress,playerPress);
+    if(playerPress === expectedPlayerPress)
+    {
+        console.log('correct');
+
+        const currentScore = getElementValueById('current-score');
+        const newScore = currentScore + 1;
+        setTextElementValueById('current-score',newScore);
+        removeBackgroundColorById(expectedPlayerPress);
+        continueGame();
+    }
+    else
+    {
+        const currentLife = getElementValueById('life');
+        const newLife = currentLife - 1;
+        if(newLife==0)
+        {
+            console.log('game over');
+            removeBackgroundColorById(expectedPlayerPress);
+            const playSection = document.getElementById('play-ground');
+            playSection.classList.add('hidden');
+            const finalScore = document.getElementById('final-score');
+            finalScore.classList.remove('hidden');
+            const finalScoreValue = getElementValueById('current-score');
+            const showScore = document.getElementById('final-score-value');
+            showScore.innerText = finalScoreValue;
+        }
+        setTextElementValueById('life',newLife);
+    }
+}
+document.addEventListener('keyup',handleKeyboardEvent);
+
 function continueGame()
 {
     const alphabet = getRandomAlphabet();
@@ -11,8 +48,12 @@ function play()
 {
     const homeSection = document.getElementById('home-screen');
     homeSection.classList.add('hidden');
+    const finalScore = document.getElementById('final-score');
+    finalScore.classList.add('hidden');
     //showing home section
     const playSection = document.getElementById('play-ground');
     playSection.classList.remove('hidden');
+    setTextElementValueById('current-score',0);
+    setTextElementValueById('life',5);
     continueGame();
 }
--- scripts/utility.js
+++ scripts/utility.js
@@ -8,12 +8,22 @@ function removeBackgroundColorById(id)
     const element = document.getElementById(id);
     element.classList.remove('bg-red-500');
 }
+function getElementValueById(elementId)
+{
+   const element = document.getElementById(elementId);
+   const elementValueString = element.innerText;
+   const elementValue = parseInt(elementValueString);
+   return elementValue;
+}
+function setTextElementValueById(elementId,elementValue)
+{
+    const element = document.getElementById(elementId);
+    element.innerText = elementValue;
+}
 function getRandomAlphabet()
 {
     const alphabetstring = "abcdefghijklmnopqrstuvwxyz";
     const alphabets = alphabetstring.split("");
-    // console.log(alphabets);
-    // get a random index 
     const randomNumber = Math.random()*25;
     const index = Math.round(randomNumber);
     const alphabet = alphabets[index];
